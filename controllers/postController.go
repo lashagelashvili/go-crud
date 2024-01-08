2539d2396a0e61514f84dbe02fc906a2d002df53
lashagelashvili <las.gelashvili@gmail.com>
2024-01-08 15:48:19 +0400

    add create post and get posts

---
+++ controllers/postController.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"go-crud/initializers"
+	"go-crud/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PostsCreate(c *gin.Context) {
+	// get data from request
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	// create a post
+	post := models.Post{Title: body.Title, Body: body.Body}
+	result := initializers.DB.Create(&post)
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// return post
+
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsIndex(c *gin.Context) {
+	// get the posts
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+
+	// return them
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
